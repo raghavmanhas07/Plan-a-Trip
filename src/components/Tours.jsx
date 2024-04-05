f446225e91924fcabe1773009178ca8b93b4e3ea
Raghav Manhas <110593911+raghavmanhas07@users.noreply.github.com>
2024-04-05 09:57:22 +0530

    Update Tours.jsx

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -10,7 +10,7 @@ const Tours = (props) => {
     return (
         <div className='container'>
             <div>
-                <h2 className='title'>Plan with Raghav</h2>
+                <h2 className='title'>Plan a trip</h2>
             </div>
             <div className='cards'>
                 {
